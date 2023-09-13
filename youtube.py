68854d0fd18e6d9f27d61016c7d8f15648391af5
Uthman Qureshi <uthman.aq@gmail.com>
2023-09-13 11:22:06 -0400

    reverted changes to section download

--- youtube.py
+++ youtube.py
@@ -3,33 +3,33 @@ from moviepy.editor import *
 import os
 import argparse
 
-def sections_callback(info_dict, ydl):
-    # Convert the global time_ranges to the format yt-dlp expects.
-    sections = []
+def download_clip(url, time_ranges, output_name_prefix):
+    # Download options for youtube_dl
+    ydl_opts = {
+        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4',
+        'outtmpl': 'temp_video.%(ext)s',
+        'merge_output_format': 'mp4',
+        'postprocessors': [{
+            'key': 'FFmpegVideoConvertor',
+            'preferedformat': 'mp4',
+        }],
+    }
+
+    # Fetch the video
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+    # Check if the merged file exists
+    if not os.path.exists('temp_video.mp4'):
+        print("Error: Video not downloaded correctly!")
+        return
+
+    # Process each time range
     for idx, (start_time, end_time) in enumerate(time_ranges):
-        section = {
-            "start_time": sum(x * int(t) for x, t in zip([3600, 60, 1], start_time)),
-            "end_time": sum(x * int(t) for x, t in zip([3600, 60, 1], end_time))
-        }
-        sections.append(section)
-    return sections
+        # Slice the video
+        main_clip = VideoFileClip('temp_video.mp4').subclip(start_time, end_time)
 
-def download_and_process_clip(url, time_ranges, output_name_prefix):
-    for idx, (start_time, end_time) in enumerate(time_ranges):
-
-        ydl_opts = {
-            'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4',
-            'outtmpl': f'temp_video_{idx}.%(ext)s',
-            'merge_output_format': 'mp4',
-            'download_ranges': sections_callback
-        }
-
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
-
-        video_path = f'temp_video_{idx}.mp4'
-        main_clip = VideoFileClip(video_path)
-
+        # Load intro and outro if they exist, and concatenate
         clips_to_concatenate = []
 
         if os.path.exists('intro.mp4'):
@@ -41,19 +41,26 @@ def download_and_process_clip(url, time_ranges, output_name_prefix):
             clips_to_concatenate.append(VideoFileClip('outro.mp4'))
 
         final_clip = concatenate_videoclips(clips_to_concatenate, method='compose')
+
+        # Generate unique output name for each clip
         output_name = f"{output_name_prefix}_{idx}.mp4"
+        # Export the concatenated video
         final_clip.write_videofile(output_name, codec="libx264")
-        os.remove(video_path)
+
+    # Remove temporary merged file
+    os.remove('temp_video.mp4')
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Download and process YouTube video sections.')
+    # Use argparse to handle command line arguments
+    parser = argparse.ArgumentParser(description='Download and process YouTube videos.')
     parser.add_argument('url', help='YouTube URL to download from.')
     parser.add_argument('time_ranges', nargs='+', help='Multiple time ranges in HH:MM:SS-HH:MM:SS format.')
     parser.add_argument('output_name_prefix', help='Prefix of the output file (e.g., output)')
+
     args = parser.parse_args()
 
-    global time_ranges
+    # Parse time_ranges
     time_ranges = [(tuple(map(int, time_range.split('-')[0].split(":"))),
                     tuple(map(int, time_range.split('-')[1].split(":")))) for time_range in args.time_ranges]
 
-    download_and_process_clip(args.url, time_ranges, args.output_name_prefix)
+    download_clip(args.url, time_ranges, args.output_name_prefix)
